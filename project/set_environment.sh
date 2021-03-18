bf39a359779bc5eb1b63eaf6ce25a3fe8bd753bc
Austin Derbique <austin@derbique.org>
2021-03-17 22:47:52 -0700

    now downloads datasets

--- project/set_environment.sh
+++ project/set_environment.sh
@@ -6,3 +6,17 @@ sudo apt install -y python3.9 python3.9-venv && \
 python3.9 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+
+echo "Obtaining Data Sets"
+mkdir -p datasets
+echo "Downloading KDD Dataset for Task 1"
+wget http://205.174.165.80/CICDataset/NSL-KDD/Dataset/NSL-KDD.zip -P datasets/
+echo "Extracting KDD Dataset for Task 1"
+unzip datasets/NSL-KDD.zip -d datasets/kdd
+
+echo "Downloading KDD Dataset for Task 3"
+wget http://205.174.165.80/CICDataset/CIC-IDS-2017/Dataset/MachineLearningCSV.zip -P datasets/
+echo "Extracting CIC-IDS-2017 Dataset"
+unzip datasets/MachineLearningCSV.zip -d datasets/ids
+rm -f datasets/{MachineLearningCSV.zip,NSL-KDD.zip}
+echo "Setup Complete"
